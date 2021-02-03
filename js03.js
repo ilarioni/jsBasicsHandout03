009f6b47824989b9cea706e6a6ca585257815c8a
ilarioni <ilia.meshvildishvili@gmail.com>
2021-02-03 13:28:20 +0100

    Nan and Infinity

---
+++ js03.js
@@ -0,0 +1,15 @@
+// Nan - not a number
+var x = 100
+var y = "Apple";
+var z = x / y;
+console.log(z);
+
+// Infinity or -Infinity
+var myNumber = 2 
+var txt = "";
+while (myNumber != Infinity) {
+    myNumber = myNumber * myNumber;
+}
+txt = txt + myNumber;
+console.log(txt);
+
